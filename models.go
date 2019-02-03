5fedbcd44bc7d8c3ea0b7fd0147f60f2e7fe1bf0
Nenad Stojanovikj <nekk1@live.com>
2019-02-03 01:58:13 +0100

    Add more watchlist endpoints

--- models.go
+++ models.go
@@ -6,27 +6,38 @@ import (
 
 type (
 	Episode struct {
-		Season    int         `json:"season"`
-		Number    int         `json:"number"`
-		Title     string      `json:"title"`
-		Providers ProviderIDs `json:"ids"`
+		Season int         `json:"season"`
+		Number int         `json:"number"`
+		Title  string      `json:"title"`
+		IDs    ProviderIDs `json:"ids"`
 	}
 
 	Show struct {
-		Title     string      `json:"title"`
-		Year      int         `json:"year"`
-		Providers ProviderIDs `json:"ids"`
+		Title string      `json:"title"`
+		Year  int         `json:"year"`
+		IDs   ProviderIDs `json:"ids"`
 	}
 
 	Movie struct {
 		Title string      `json:"title"`
 		Year  int         `json:"year"`
-		Ids   ProviderIDs `json:"ids"`
+		IDs   ProviderIDs `json:"ids"`
+	}
+
+	Season struct {
+		Number int         `json:"number"`
+		IDs    ProviderIDs `json:"ids"`
 	}
 
 	ShowEpisode struct {
 		FirstAired time.Time `json:"first_aired"`
 		Episode    Episode   `json:"episode"`
+		Show       Show      `json:"show"`
+	}
+
+	ShowSeason struct {
+		FirstAired time.Time `json:"first_aired"`
+		Season     Season    `json:"season"`
 		Show       Show      `json:"show"`
 	}
 
@@ -36,6 +47,7 @@ type (
 		IMDb   string `json:"imdb"`
 		TMDB   int    `json:"tmdb"`
 		TVRage int    `json:"tvrage"`
+		Slug   string `json:"slug"`
 	}
 
 	// Metadata coming from Watchlist endpoint
@@ -59,5 +71,20 @@ type (
 		ListedAt time.Time `json:"listed_at"`
 		Type     string    `json:"type"`
 		Show     Show      `json:"show"`
+	}
+
+	MetadataSeason struct {
+		Rank     int       `json:"rank"`
+		ListedAt time.Time `json:"listed_at"`
+		Type     string    `json:"type"`
+		Season   Season    `json:"season"`
+		Show     Show      `json:"show"`
+	}
+
+	FullMetadata struct {
+		Movies   []Movie   `json:"movies"`
+		Shows    []Show    `json:"shows"`
+		Seasons  []Season  `json:"seasons"`
+		Episodes []Episode `json:"episodes"`
 	}
 )
--- user.go
+++ user.go
@@ -1,8 +1,10 @@
 package trakt
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -23,6 +25,22 @@ func (t *Client) WatchlistMovies() (movies []MetadataMovie, err error) {
 	return movies, err
 }
 
+// TODO Add options for methods for sorting etc
+func (t *Client) WatchlistSeasons() (seasons []MetadataSeason, err error) {
+	err = t.get(fmt.Sprintf("https://api.trakt.tv/sync/watchlist/seasons"), &seasons)
+	return seasons, err
+}
+
+// TODO Add options for methods for sorting etc
+func (t *Client) WatchlistShows() (shows []MetadataShow, err error) {
+	err = t.get(fmt.Sprintf("https://api.trakt.tv/sync/watchlist/shows"), &shows)
+	return shows, err
+}
+
+func (t *Client) RemoveFromWatchlist(m FullMetadata) error {
+	return t.post(fmt.Sprintf("https://api.trakt.tv/sync/watchlist/remove"), m, nil)
+}
+
 func (t *Client) get(url string, response interface{}) error {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -34,7 +52,37 @@ func (t *Client) get(url string, response interface{}) error {
 		return err
 	}
 
-	err = json.NewDecoder(resp.Body).Decode(&response)
+	if response != nil {
+		err = json.NewDecoder(resp.Body).Decode(&response)
+	}
+
+	return err
+}
+
+func (t *Client) post(url string, body interface{}, response interface{}) error {
+	b, err := json.Marshal(body)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(b))
+	if err != nil {
+		return err
+	}
+
+	resp, err := t.HttpClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != 200 {
+		body, _ := ioutil.ReadAll(resp.Body)
+		return fmt.Errorf("error while removing from watchlist: %s", string(body))
+	}
+
+	if response != nil {
+		err = json.NewDecoder(resp.Body).Decode(&response)
+	}
 
 	return err
 }
